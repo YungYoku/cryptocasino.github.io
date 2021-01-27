a5b3d4ca582c27c4e364fdc89ff7b385f1274051
YungYoku <63597891+YungYoku@users.noreply.github.com>
2021-01-27 10:29:26 +0000

    Add files via upload

---
+++ scripts/exchanger.js
@@ -0,0 +1,13 @@
+let tempo = document.querySelector("#coin_11");
+function coinChanger(coin) {
+    tempo.style.boxShadow = "0 0 7px 1px #1396cb";
+    coin.style.boxShadow = "0 0 7px 2px #3aeb7e";
+    tempo = coin;
+}
+
+let tempo2 = document.querySelector("#coin_21");
+function coinChanger2(coin) {
+    tempo2.style.boxShadow = "0 0 7px 1px #1396cb";
+    coin.style.boxShadow = "0 0 7px 2px #3aeb7e";
+    tempo2 = coin;
+}
