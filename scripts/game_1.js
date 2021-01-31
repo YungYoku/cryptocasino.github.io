5c4dba15db4034086850b4e7df6f2dd0e8db2cd2
YungYoku <63597891+YungYoku@users.noreply.github.com>
2021-01-31 09:11:55 +0000

    Add files via upload

--- scripts/game_1.js
+++ scripts/game_1.js
@@ -1,29 +1,30 @@
-let game1_EOS = document.querySelector("#game_1_window__levels__EOS");
 let game1_BCH = document.querySelector("#game_1_window__levels__BCH");
 let game1_XRP = document.querySelector("#game_1_window__levels__XRP");
 let game1_LTC = document.querySelector("#game_1_window__levels__LTC");
 let game1_ETH = document.querySelector("#game_1_window__levels__ETH");
 let game1_BTC = document.querySelector("#game_1_window__levels__BTC");
 
-let game1_tokens = [game1_EOS, game1_BCH, game1_XRP, game1_LTC, game1_ETH, game1_BTC];
+let game1_tokens = [game1_BCH, game1_XRP, game1_LTC, game1_ETH, game1_BTC];
+
+let game_1_window__game__1 = document.querySelector("#game_1_window__game__1");
+let game_1_window__game__2 = document.querySelector("#game_1_window__game__2");
+let game_1_window__game__3 = document.querySelector("#game_1_window__game__3");
+let game_1_window__game__4 = document.querySelector("#game_1_window__game__4");
+let game_1_window__game__5 = document.querySelector("#game_1_window__game__5");
+let game_1_window__game__6 = document.querySelector("#game_1_window__game__6");
+let game_1_window__game = document.querySelector("#game_1_window__game");
+let game_1_window__game__wrap = document.querySelector("#game_1_window__game__wrap");
 
 let levelTempo = 1;
 let game1FirstClickOnLevel = true;
 function game_1_levelsChanger(level) {
     if(game1FirstClickOnLevel) {
-        levelTempo = level - 1;
+        if(level === 1) levelTempo = 5;
+        else if(level === 5) levelTempo = 1;
         game1FirstClickOnLevel = false;
     }
     switch(level) {
         case 1:
-            game1_EOS.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
-            game1_EOS.style.border = "none";
-            game1_EOS.style.transform = "scale(1.2)";
-            game1_tokens[levelTempo-1].style.boxShadow = "none";
-            game1_tokens[levelTempo-1].style.border = "3px solid #1396cb";
-            game1_tokens[levelTempo-1].style.transform = "none";
-            break;
-        case 2:
             game1_BCH.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
             game1_BCH.style.border = "none";
             game1_BCH.style.transform = "scale(1.2)";
@@ -31,15 +32,22 @@ function game_1_levelsChanger(level) {
             game1_tokens[levelTempo-1].style.border = "3px solid #1396cb";
             game1_tokens[levelTempo-1].style.transform = "none";
             break;
-        case 3:
+        case 2:
             game1_XRP.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
             game1_XRP.style.border = "none";
             game1_XRP.style.transform = "scale(1.2)";
             game1_tokens[levelTempo-1].style.boxShadow = "none";
             game1_tokens[levelTempo-1].style.border = "3px solid #1396cb";
             game1_tokens[levelTempo-1].style.transform = "none";
+            
+            game_1_window__game.style.display = "grid";
+            game_1_window__game.style.width = "470px";
+            game_1_window__game.style.gridTemplate = "auto / 1fr 1fr 1fr";
+            game_1_window__game.style.gridTemplateAreas = "'game_1_window__game__1 game_1_window__game__2 game_1_window__game__3'";
+            game_1_window__game__3.style.display = "grid";
+            game_1_window__game__wrap.style.width = "570px";
             break;
-        case 4:
+        case 3:
             game1_LTC.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
             game1_LTC.style.border = "none";
             game1_LTC.style.transform = "scale(1.2)";
@@ -47,7 +55,7 @@ function game_1_levelsChanger(level) {
             game1_tokens[levelTempo-1].style.border = "3px solid #1396cb";
             game1_tokens[levelTempo-1].style.transform = "none";
             break;
-        case 5:
+        case 4:
             game1_ETH.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
             game1_ETH.style.border = "none";
             game1_ETH.style.transform = "scale(1.2)";
@@ -55,7 +63,7 @@ function game_1_levelsChanger(level) {
             game1_tokens[levelTempo-1].style.border = "3px solid #1396cb";
             game1_tokens[levelTempo-1].style.transform = "none";
             break;
-        case 6:
+        case 5:
             game1_BTC.style.boxShadow = "0 0 7px 4px rgb(58, 235, 126)";
             game1_BTC.style.border = "none";
             game1_BTC.style.transform = "scale(1.2)";
